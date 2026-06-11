3eefabe2ef97eb42b593431fb6a78c4a63c1b157
srayanayt <g7bv2669rf@privaterelay.appleid.com>
2026-06-11 20:10:47 +0100

    Refactor hyper.py for improved structure and updates

--- hyper.py
+++ hyper.py
@@ -12,7 +12,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
 
 # --- CONFIGURATION & VERSION CONTROL ---
-ONLINE_FILE_URL = "https://srayanayt.github.io/hyperproxysite/"
+ONLINE_FILE_URL = "https://srayanayt.github.io/hyperproxysite/hyper.py"
 PORT = 8080
 
 # --- PATH SETUP ---
